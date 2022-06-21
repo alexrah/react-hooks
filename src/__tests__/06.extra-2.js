457c1c494f5e213c89f06d87d2473c5826f83a9b
alessandro stoppato <alexrah@gmail.com>
2022-06-21 14:43:47 +0200

    src/__tests__/06.extra-2.js: testing statuses

--- src/__tests__/06.extra-2.js
+++ src/__tests__/06.extra-2.js
@@ -9,15 +9,25 @@ beforeEach(() => jest.spyOn(window, 'fetch'))
 afterEach(() => window.fetch.mockRestore())
 
 test('displays the pokemon', async () => {
-  render(<App />)
+  const {container} = render(<App />)
   const input = screen.getByLabelText(/pokemon/i)
   const submit = screen.getByText(/^submit$/i)
+
+  const statusElement  = container.querySelector('pre');
+
+  expect(statusElement.textContent).toEqual('idle');
 
   // verify that an initial request is made when mounted
   await userEvent.type(input, 'pikachu')
   await userEvent.click(submit)
 
-  await screen.findByRole('heading', {name: /pikachu/i})
+  expect(window.fetch).toHaveBeenCalled();
+  expect(statusElement.textContent).toEqual('pending');
+
+  await screen.findByRole('heading', {name: /pikachu/i});
+  await screen.findByText(/resolved/i);
+
+  expect(statusElement.textContent).toEqual('resolved');
 
   // verify that a request is made when props change
   await userEvent.clear(input)
@@ -43,5 +53,6 @@ test('displays the pokemon', async () => {
   await userEvent.click(submit)
   const errMsg = await screen.findByText(/There was an error:/i);
   expect(errMsg).toBeInTheDocument();
+  expect(statusElement.textContent).toEqual('rejected');
 
 })
